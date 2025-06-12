d2758485b8f02e618d7afa4e8072ac3bcf201c23
paddymac <mcnabb_patrick@yahoo.com>
2025-06-12 22:30:28 +0100

    Updated testnet info

--- deploy.js
+++ deploy.js
@@ -1,5 +1,13 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+const { interface, bytecode } = require("./compile");
 
-// deploy code will go here
+//updated web3 and hdwallet-provider imports added for convenience
+// truffle.HD allows us access to the MetaMask wallet via the mnemonic, and testnet to deploy the contract to
+const provider = new HDWalletProvider(
+    'siege hat ghost people equal carpet payment bounce project connect rather galaxy',
+    'https://sepolia.infura.io/v3/63f41e7ac135498f9df0337d97f176de'
+);
+const web3 = new Web3(provider); // web3 constructor with the testnet and wallet account details
+
+// deploy script to Sepolia testnet
