4729795f5b740a5356fddb9ca516b54ac80ce0fb
paddymac <mcnabb_patrick@yahoo.com>
2025-06-13 22:25:21 +0100

    Contract deployed on Ethereum Sepolia testnet

--- deploy.js
+++ deploy.js
@@ -11,3 +11,16 @@ const provider = new HDWalletProvider(
 const web3 = new Web3(provider); // web3 constructor with the testnet and wallet account details
 
 // deploy script to Sepolia testnet
+const deploy = async () => {  // wrapping asyc in higer level function
+  const accounts = await web3.eth.getAccounts();  // single mne can generate many accounts
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);  // contract address on testnet
+  provider.engine.stop();
+};
+deploy();   // call the function
